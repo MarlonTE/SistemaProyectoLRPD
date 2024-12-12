bac0613976d2439e818f526ce1cdc293344a824a
marlon.its.me <marlonte81@gmail.com>
2024-12-12 16:27:02 -0500

    Comandos Login ViewModel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SistemaProyectoLRPD.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Campos
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        //Propiedades
+        public string Username { get { return _username; } set { _username = value; OnPropertyChanged(nameof(Username)); } }
+        public SecureString Password { get { return _password; } set { _password = value; OnPropertyChanged(nameof(Password)); } }
+        public string ErrorMessage { get { return _errorMessage; } set { _errorMessage = value; OnPropertyChanged(nameof(ErrorMessage)); } }
+        public bool IsViewVisible { get { return _isViewVisible; } set { _isViewVisible = value; OnPropertyChanged(nameof(IsViewVisible)); } }
+
+        //Icommands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordComand { get; }
+
+        //Constructores
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p=> ExecuteRecoverPasswordCommand("", ""));
+        }
+
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool ValidData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+            {
+                ValidData = false;
+            }
+            else
+            {
+                ValidData = true;
+            }
+            return ValidData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        private void ExecuteRecoverPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
