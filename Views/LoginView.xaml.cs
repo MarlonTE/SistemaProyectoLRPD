ad694d9fe692b4ae86036e8cc80b45c1d8c5c4f0
marlon.its.me <marlonte81@gmail.com>
2024-12-12 12:09:52 -0500

    imagen y login view

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -20,5 +20,20 @@ namespace SistemaProyectoLRPD
         {
             InitializeComponent();
         }
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void bttn_close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void bttn_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
     }
 }
